8b5975ab6fc5d9a6353a45c6f332e00e3075dbd2
Jano Simas <janosimas@gmail.com>
2018-03-04 21:36:02 -0300

    ordering treasure cards in a sane way

--- src/cards/base_treasure.js
+++ src/cards/base_treasure.js
@@ -7,20 +7,22 @@ const base_treasure = [{
   count: 10,
   type: [types.TREASURE],
   image: 'http://wiki.dominionstrategy.com/images/thumb/f/fb/Copper.jpg/200px-Copper.jpg'
-}, {
-  name: 'Gold',
-  cost: 6,
-  treasure: 3,
-  count: 10,
-  type: [types.TREASURE],
-  image: 'http://wiki.dominionstrategy.com/images/thumb/5/50/Gold.jpg/200px-Gold.jpg'  
-}, {
+},
+{
   name: 'Silver',
   cost: 3,
   treasure: 2,
   count: 10,
   type: [types.TREASURE],
   image: 'http://wiki.dominionstrategy.com/images/thumb/5/5d/Silver.jpg/200px-Silver.jpg'
+},
+{
+  name: 'Gold',
+  cost: 6,
+  treasure: 3,
+  count: 10,
+  type: [types.TREASURE],
+  image: 'http://wiki.dominionstrategy.com/images/thumb/5/50/Gold.jpg/200px-Gold.jpg'
 }];
 
 export default base_treasure;
