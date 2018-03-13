deb631170cd2fcabf6d7b80e8f3fd04de8e55771
Jano Simas <janosimas@gmail.com>
2018-03-12 21:48:28 -0300

    add curse card

---
+++ src/games/dominion/base/cards/curse.js
@@ -0,0 +1,17 @@
+import types from '../../cardTypes';
+import React from 'react';
+
+const card = {
+  name: "Curse",
+  back: <img src='http://wiki.dominionstrategy.com/images/c/ca/Card_back.jpg' alt='Deck' />,
+  front: <img src='http://wiki.dominionstrategy.com/images/thumb/9/97/Curse.jpg/200px-Curse.jpg' alt="Curse" />,
+  isFaceUp: true,
+  canHover: true,
+  count: 10,
+  cost: 0,
+  treasure: -1,
+  type: [types.TREASURE],
+  className: 'card',
+};
+
+export default card;
