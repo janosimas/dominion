3da89bc05fb20beb022abfed39bb16620f5ce555
Jano Simas <janosimas@gmail.com>
2018-04-14 21:34:14 -0300

    throne room

--- src/games/dominion/core/cards/throne_room.js
+++ src/games/dominion/core/cards/throne_room.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import types from '../../cardTypes'
-import { currentPlayer, getState } from '../../../utils'
-import { playCard, playCopy, getLastPhase, popPhase, pushPhase } from '../../utils';
+import types from '../../cardTypes';
+import { currentPlayer, getState } from '../../../utils';
+import { playCard, playCopy, getLastPhase, popPhase, pushPhase, getTopPhase } from '../../utils';
 
-const CUSTOM_PHASE = 'Throne Room play phase';
+const PLAY_PHASE = 'Throne Room play phase';
+const COPY_PHASE = 'Throne Room copy phase';
 
 const card = {
-  name: "Throne Room",
+  name: 'Throne Room',
   back: <img src='http://wiki.dominionstrategy.com/images/c/ca/Card_back.jpg' alt='Deck' />,
   front: <img src='http://wiki.dominionstrategy.com/images/thumb/d/d1/Throne_Room.jpg/200px-Throne_Room.jpg' alt="Throne Room" />,
   isFaceUp: true,
@@ -18,46 +19,60 @@ const card = {
   type: [types.ACTION],
   onPlay: (G, ctx) => {
     const state = getState(G);
-    pushPhase(state, CUSTOM_PHASE);
-    state.custom_onClickHand = (G, ctx, index) => {
-      if (ctx.phase !== CUSTOM_PHASE) {
-        return G;
-      }
-
-      let state = getState(G);
-      const player = currentPlayer(state, ctx);
-      if (!player.hand[index].type.includes(types.ACTION)) {
-        return state;
-      }
-
-      const card = player.hand.splice(index, 1)[0];
-      state = playCard(state, ctx, card);
-      state = playCopy(state, ctx, card);
-
-      state.end_custom_phase = true;
-      return state;
-    };
-
-    state.onHighlightHand = (G, ctx, card) => {
-      if (card.type.includes(types.ACTION)) {
-        return ' highlight';
-      }
-      return '';
-    };
-
-    state.allowEndPhase = () => {
-      return getLastPhase(state);
-    };
-
+    pushPhase(state, PLAY_PHASE);
+    
     return state;
   },
   custom_moves: [],
   custom_phases: [
     {
-      name: CUSTOM_PHASE,
+      name: PLAY_PHASE,
       allowedMoves: ['onClickHand'],
+      onPhaseBegin: (G, ctx) => {
+        const state = getState(G);
+        const player = currentPlayer(state, ctx);
+        state.custom_onClickHand = (G, ctx, index) => {
+          if (ctx.phase !== PLAY_PHASE) {
+            return G;
+          }
+
+          let state = getState(G);
+          const player = currentPlayer(state, ctx);
+          if (!player.hand[index].type.includes(types.ACTION)) {
+            return state;
+          }
+
+          popPhase(state);
+          pushPhase(state, COPY_PHASE);
+
+          state.throne_card = player.hand.splice(index, 1)[0];
+          state = playCard(state, ctx, state.throne_card);
+          
+          return state;
+        };
+
+        state.onHighlightHand = (G, ctx, card) => {
+          if (card.type.includes(types.ACTION)) {
+            return ' highlight';
+          }
+          return '';
+        };
+
+        state.end_custom_phase = true;
+        for(const card of player.hand) {
+          if(card.type.includes(types.ACTION)) {
+            state.end_custom_phase = false;
+            break;
+          }
+        }
+
+        return state;
+      },
+
       endPhaseIf: (G, ctx) => {
-        if (G.end_custom_phase) {
+        if (G.throne_card) {
+          return getTopPhase(G);
+        } else if (G.end_custom_phase) {
           return getLastPhase(G);
         }
 
@@ -66,12 +81,37 @@ const card = {
       onPhaseEnd: (G, ctx) => {
         const state = getState(G);
 
-        G.end_custom_phase = undefined;
-        state.allowEndPhase = undefined;
         state.custom_onClickHand = undefined;
         state.onHighlightHand = undefined;
-        popPhase(state);
+
+        if (state.end_custom_phase) {
+          popPhase(state);
+        }
+
+        state.end_custom_phase = undefined;
+
         return state;
+      }
+    },
+    {
+      name: COPY_PHASE,
+      allowedMoves: [],
+      onPhaseBegin: (G, ctx) => {
+        let state = getState(G);
+
+        popPhase(state);
+        state = playCopy(state, ctx, state.throne_card);
+        state.throne_card = undefined;
+
+        return state;
+      },
+
+      endPhaseIf: (G, ctx) => {
+        if (!G.throne_card) {
+          return getTopPhase(G);
+        }
+
+        return false;
       }
     }
   ]
