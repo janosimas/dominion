fd6821631a2f827a7deae0ec1257b5ef56704226
Jano Simas <janosimas@gmail.com>
2018-04-14 21:58:38 -0300

    fix style

--- src/games/dominion/core/cards/village.js
+++ src/games/dominion/core/cards/village.js
@@ -1,19 +1,19 @@
-import types from '../../cardTypes'
+import types from '../../cardTypes';
 import React from 'react';
 
 
 const card = {
-    name: "Village",
-    back: <img src='http://wiki.dominionstrategy.com/images/c/ca/Card_back.jpg' alt='Deck' />,
-    front: <img src='http://wiki.dominionstrategy.com/images/thumb/5/5a/Village.jpg/200px-Village.jpg' alt="Village" />,
-    isFaceUp: true,
-    canHover: true,
-    count: 10,
-    cost: 3,
-    cards: 1,
-    actions: 2,
-    type: [types.ACTION],
+  name: 'Village',
+  back: <img src='http://wiki.dominionstrategy.com/images/c/ca/Card_back.jpg' alt='Deck' />,
+  front: <img src='http://wiki.dominionstrategy.com/images/thumb/5/5a/Village.jpg/200px-Village.jpg' alt="Village" />,
+  isFaceUp: true,
+  canHover: true,
+  count: 10,
+  cost: 3,
+  cards: 1,
+  actions: 2,
+  type: [types.ACTION],
   className: 'card',
-  };
+};
 
 export default card;
