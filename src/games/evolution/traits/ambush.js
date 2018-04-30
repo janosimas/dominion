21349eaac64cd126f6714a150c97a39129f89ab8
Jano Simas <janosimas@gmail.com>
2018-04-30 16:20:06 -0300

    ambush card

--- src/games/evolution/traits/ambush.js
+++ src/games/evolution/traits/ambush.js
@@ -1,18 +1,19 @@
 import Trait from './trait';
-// import types from '../../traitTypes';
 
 
-const name = 'Ambush';
-const types = [];
+class Ambush extends Trait {
+  constructor(food) {
+    super('Ambush', [], food);
+  }
+}
 
 const AmbushCards = [];
-AmbushCards.push(new Trait(name, types, -3));
-AmbushCards.push(new Trait(name, types, -2));
-AmbushCards.push(new Trait(name, types, -1));
-AmbushCards.push(new Trait(name, types, 0));
-AmbushCards.push(new Trait(name, types, 1));
-AmbushCards.push(new Trait(name, types, 2));
-AmbushCards.push(new Trait(name, types, 3));
-
+AmbushCards.push(new Ambush(3));
+AmbushCards.push(new Ambush(2));
+AmbushCards.push(new Ambush(1));
+AmbushCards.push(new Ambush(0));
+AmbushCards.push(new Ambush(1));
+AmbushCards.push(new Ambush(2));
+AmbushCards.push(new Ambush(3));
 
 export default AmbushCards;
